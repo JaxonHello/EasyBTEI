e6a9cfb910f7c2297b6e764fd30968b486d17770
Jaxon He <113959039+JaxonHello@users.noreply.github.com>
2024-04-22 19:10:14 +0800

    Update human_gl_db_install.sh

--- scritps/human_gl_db_install.sh
+++ scritps/human_gl_db_install.sh
@@ -1,33 +1,33 @@
-#! /bin/bash
-
-cd ncbi-igblast-1.22.0
+####################################
+## 下载human参考序列
 cd ref_seq
 mkdir human
 cd human
 
-# 从网站上下载human IMGT VDJ reference fasta file
+# 从网站上下载human IMGT VDJ参考序列
 wget -r -np -nH --cut-dirs=10 --accept '*.fasta'  https://www.imgt.org/download/V-QUEST/IMGT_V-QUEST_reference_directory/Homo_sapiens/IG/
 
-# 在文件名前加human前缀
+# 在文件名前面加human前缀
 for file in *.fasta; do
     mv "$file" "human_$file"
 done
 
-# 将Heavy, Kappa, Lambda的V, J合并起来
+# 将Heavy, Kappa, Lambda的V,(D),J序列合并起来
 cat *V*.fasta > human_V.fasta
 cat *D*.fasta > human_D.fasta
 cat *J*.fasta > human_J.fasta
 
-# 由于C区数据库无法下载，请参考获得完整的C区germline reference seq
-# https://www.imgt.org/genedb/fastaC.action
-# 从github下载已经我们自己构建好的C区fasta文件(由于长城防火墙的存在会导致下载失败)
-wget https://github.com/JaxonHello/EasyBTEI/blob/main/resource/IMGT_human_gl_C.fasta
-mv IMGT_human_gl_C.fasta human_C.fasta && rm IMGT_human_gl_C.fasta
+# 下载C区序列
+# 1. 从github上下载
+# wget https://github.com/JaxonHello/EasyBTEI/blob/main/resource/IMGT_human_gl_C.fasta
+# 2. 如果github无法下载，请下载到本地文件夹并进行以下处理
+# 确保在本地的序列位置在与igblast同级别的source文件夹
+mv ../../../source/IMGT_human_gl_C.fasta human_C.fasta
 
-# 处理human_C.fasta的序列名称
+# 处理mouse_C.fasta的序列名称，只留下IGHG*01这样的类型
 awk -F '|' '/^>/ {sub(/^>.*/, ">"$2)} 1' human_C.fasta > tmp && mv tmp human_C.fasta
 
-# 处理human_C.fasta的重复序列，给重复序列命名
+# 处理mouse_C.fasta中的重复序列，使其变成IGHG1*01-1这样的形式
 awk '/^>/ {
     if ($0 in seen) {
         seen[$0]++;
@@ -39,27 +39,21 @@ awk '/^>/ {
 } 
 {print}' human_C.fasta > tmp && mv tmp human_C.fasta
 
-
-# 开始构建数据库
-
-# 将germline序列移动到database/human文件夹
-# 在database下创建human dir
+## 构建human数据库
 cd ../../database
 mkdir human
-# 回到igblast dir 
 cd ../
 
-# 移动并预处理germline seq
+# 移动并处理germline seq
 bin/edit_imgt_file.pl ref_seq/human/human_V.fasta > database/human/human_V.fasta
 bin/edit_imgt_file.pl ref_seq/human/human_D.fasta > database/human/human_D.fasta
 bin/edit_imgt_file.pl ref_seq/human/human_J.fasta > database/human/human_J.fasta
 bin/edit_imgt_file.pl ref_seq/human/human_C.fasta > database/human/human_C.fasta
 
-# 转换为数据库格式
+# 扩展为数据库格式
 bin/makeblastdb -parse_seqids -dbtype nucl -in database/human/human_V.fasta
 bin/makeblastdb -parse_seqids -dbtype nucl -in database/human/human_D.fasta
 bin/makeblastdb -parse_seqids -dbtype nucl -in database/human/human_J.fasta
 bin/makeblastdb -parse_seqids -dbtype nucl -in database/human/human_C.fasta
 
-# 成功构建human germline VDJC数据库
 echo "succeed in installing human germline VDJC database in IgBLAST!!"
